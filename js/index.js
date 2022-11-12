9715557e2404e3ad207ff3bb15098e9ccfded10e
Nicole Witomski <nwitomski@udesa.edu.ar>
2022-11-12 19:22:54 -0300

    cambios en el home, responsive terminado

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let apiKey = "399cd9827f714613d04693cee425808c"
 
+//VALIDANDO EL FORMULARIO
 window.addEventListener("load",function(){
     let form = document.querySelector(".formulario")
 
@@ -32,6 +33,8 @@ function mandarFomrulario(evento){
     }
 }
 
+//ARAMANDO EL HOME 
+
 let container1 = document.querySelector(".categoria1")
 let movies = " "
 
@@ -43,9 +46,8 @@ fetch(`https://api.themoviedb.org/3/movie/popular?api_key=399cd9827f714613d04693
     for (i=0; i< 5;i++){
         movies += `<article class="articulo">
     <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt='${data.results[i].title}' />
-    <h2 class="titulocategorias"> ${data.results[i].title} </h2>
+    <p class="titulocategorias"> ${data.results[i].title} </p>
     <p class="fecha"> ${data.results[i].release_date }</p>
-    <button class="boton_ver_ahora" type="submit"> <a class="verahora" href="./detail-movie.html"> VER AHORA </a> </button>
     </article>`
     }
     container1.innerHTML = movies
@@ -66,9 +68,8 @@ fetch(`https://api.themoviedb.org/3/tv/top_rated?api_key=399cd9827f714613d04693c
     for (i=0; i< 5;i++){
         series += `<article class="articulo">
     <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt='${data.results[i].title}' />
-    <h2 class="titulocategorias"> ${data.results[i].name} </h2>
+    <p class="titulocategorias"> ${data.results[i].name} </p>
     <p class="fecha"> ${data.results[i].first_air_date}</p>
-    <button class="boton_ver_ahora" type="submit"> <a class="verahora" href="./detail-movie.html"> VER AHORA </a> </button>
     </article>`
     }
     container2.innerHTML = series
@@ -86,15 +87,22 @@ fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=399cd9827f714613d046
 })
 .then(function(data){
     for (i=0; i< 5;i++){
-        movies2 += `<article class="articulo">
-    <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt='${data.results[i].title}' />
-    <h2 class="titulocategorias"> ${data.results[i].title} </h2>
-    <p class="fecha"> ${data.results[i].release_date}</p>
-    <button class="boton_ver_ahora" type="submit"> <a class="verahora" href="./detail-movie.html"> VER AHORA </a> </button>
-    </article>`
+        movies2 += `<article class="articulo"> 
+        <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt='${data.results[i].title}' />
+        <p class="titulocategorias"> ${data.results[i].title} </p>
+        <p class="fecha"> ${data.results[i].release_date}</p>
+        </article>`
     }
     container3.innerHTML = movies2
 })
 .catch (function(error){
     console.log(error)
 })
+
+//AL HACER CLICK QUE MANDE A DETALLES
+
+/*<a href="./detail-movie.html?id=${data.results[i].id}> 
+   </a>
+   
+    // <button class="boton_ver_ahora" type="submit"> <a class="verahora" href="./detail-movie.html"> VER AHORA </a> </button>
+    */
