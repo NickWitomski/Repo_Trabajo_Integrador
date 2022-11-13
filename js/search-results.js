bd2f2f49ed3f492a3358c933209e0b267fcc3265
mpintoescalier <mpintoescalier@uedsa.edu.ar>
2022-11-13 20:15:10 -0300

    generos

--- js/search-results.js
+++ js/search-results.js
@@ -4,13 +4,12 @@ window.addEventListener('load',function(){
 
 let container = document.querySelector(".section_pel")
 let movie =location.search
-let movies = []
+let movies = " "
 console.log(location)
 let objMovie = new URLSearchParams(movie)
 let keyword = objMovie.get('name')
 let series = ' '
 let container2 = document.querySelector(".section_ser")
-let peliculass = " "
 
 
 
@@ -18,28 +17,29 @@ fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${keywo
 .then(function(resp){
     return resp.json()
 })
+
 .then(function(data){
     for (i=0; i< 5;i++){
         if (data.results[i].media_type === 'movie'){
-            if(movies.length<5 && movies.length> 0){
-                let pelicula = data.results[i].title
-                movies.push(pelicula)
-            }}}  
-    for (m=0; m<movie.length;m++){
-        peliculass += `<article class="articulo">
+        movies += `<article class="articulo">
         <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt='${data.results[i].title}' />
         <h2 class="titulocategorias"> ${data.results[i].title} </h2>
         <p class="fecha"> ${data.results[i].release_date }</p>
         </article>`
-    } 
-    container.innerHTML = movies
-    
+        console.log(data)
+} 
+    container.innerHTML = movie
+.then(function(error){
+    console.log(error)
+
 })
+
 
 fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${keyword}`)
 .then(function(resp){
     return resp.json()
 })
+
 .then(function(data){
     for (i=0; i< 5;i++){
         if (data.results[i].media_type === 'tv'){
@@ -52,7 +52,6 @@ fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${keywo
         
     }}
     container2.innerHTML = series
-})
 
 .then(function(error){
     console.log(error)
