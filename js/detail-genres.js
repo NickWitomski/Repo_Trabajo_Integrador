d5603d34ab4f8d551ccae6074ccc4cbe9facc649
Bautista Porcel <bporcel@udesa.edu.ar>
2022-11-18 15:43:10 -0300

    generos listo

--- js/detail-genres.js
+++ js/detail-genres.js
@@ -35,12 +35,12 @@ function formValidation(form,input){
 
 
 // window.addEventListener('load',function(){
-let container = document.querySelector(".categoria")
-let movie =location.search
-console.log(location)
-let objMovie = new URLSearchParams(movie)
-let keyword = objMovie.get('name')
-let series = ' '
+// let container = document.querySelector(".categoria")
+// let movie =location.search
+// console.log(location)
+// let objMovie = new URLSearchParams(movie)
+// let keyword = objMovie.get('name')
+// let series = ' '
 
 // fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${keyword}`)
 // .then(function(resp){
@@ -64,25 +64,3 @@ let series = ' '
 // })
 // })
 
-fetch(`https://api.themoviedb.org/3/search/multi?api_key=399cd9827f714613d04693cee425808c&language=en-US&page=1&include_adult=false&query=${keyword}`)
-.then(function(resp){
-    return resp.json()
-})
-.then(function(data){
-    console.log(data)
-    for (i=0; i< 5;i++){
-            series += `<article class="articulo">
-        <img class="imagen" src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" alt='${data.results[i].original_title}' />
-        <h2 class="titulocategorias"> ${data.results[i].title} </h2>
-        <p class="fecha"> ${data.results[i].first_air_date }</p>
-        </article>`
-        console.log(data)
-        
-    }
-    container.innerHTML = series
-})
-
-.catch(function(error){
-    console.log(error)
-})
-// })
