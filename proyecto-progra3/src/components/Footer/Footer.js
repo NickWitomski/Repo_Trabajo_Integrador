2881b9e6603fe693bc8a2ad0f926c3a14691f8e9
Nicole Witomski <nwitomski@udesa.edu.ar>
2023-08-28 17:03:23 -0300

    cambio

--- proyecto-progra3/src/components/Footer/Footer.js
+++ proyecto-progra3/src/components/Footer/Footer.js
@@ -13,6 +13,7 @@ function Footer(){
                     <p class="texto1"> Marcos Pinto Escalier, Bautista Porcello y  Nicole Witomski</p> 
                     
                     <ul class="contacto">
+                        
                         <li class="icono"><i class="fa-brands fa-whatsapp"></i>  </li>
                         <li class="icono"><i class="fa-regular fa-envelope"></i> </li>
                         <li class="icono"> <i class="fa-brands fa-instagram"> </i> </li>
